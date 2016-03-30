973179c1d0407f0e82e3c9d4f8cf9f3c0fc0bb71
kingsmiler <kingsmiler@163.com>
2016-03-30 22:20:14 +0800

    1、更新 listing34.go，增加不使用代理的演示； 2、更新 listing35.go，拷贝缓冲区的示例，值类型与指针类型的使用说明等。

--- goinaction-chapter5/src/listing34/listing34.go
+++ goinaction-chapter5/src/listing34/listing34.go
@@ -18,6 +18,8 @@ func init() {
 
 // 程序主入口。
 func main() {
+	// Proxy 为 nil 表示不使用代理.
+	// 默认情况下, 如果当前环境中配置了代理,那所有的GO请求也是通过了代理的,除非指定不使用代理.
 	transport := &http.Transport{Proxy: nil}
 	client := &http.Client{Transport: transport}
 
@@ -27,6 +29,7 @@ func main() {
 	checkError(err)
 	fmt.Println("Read ok")
 
+	// 响应码
 	if response.Status != "200 OK" {
 		fmt.Println(response.Status)
 		os.Exit(2)
@@ -34,17 +37,20 @@ func main() {
 	fmt.Println("Reponse ok")
 
 	var buf [512]byte
+	// 响应体为一个可读取可关闭的输入流
 	reader := response.Body
 	for {
+		// 缓冲读取, n 为读取的字节数
 		n, err := reader.Read(buf[0:])
 		if err != nil {
-			os.Exit(0)
+			reader.Close()
+			break
 		}
 		fmt.Print(string(buf[0:n]))
 	}
 
+	// 正常退出
 	os.Exit(0)
-
 }
 
 func checkError(err error) {
--- goinaction-chapter5/src/listing35/listing35.go
+++ goinaction-chapter5/src/listing35/listing35.go
@@ -1,5 +1,4 @@
-// Sample program to show how a bytes.Buffer can also be used
-// with the io.Copy function.
+// 本代码演示如何将字节缓冲区内容输出.
 package main
 
 import (
@@ -9,16 +8,31 @@ import (
 	"os"
 )
 
-// main is the entry point for the application.
 func main() {
+	// 定义字节缓冲区.
 	var b bytes.Buffer
 
-	// Write a string to the buffer.
+	// 向缓冲区中填充字符串, 字节数组;
+	// Write 为缓冲区的方法, 由于需要修改内容, 要求的对象是指针;
+	// 结构体中的自身的方法自动转换值类型与引用类型;
+	// 对象为值类型还是引用类型在定义时已确定.
 	b.Write([]byte("Hello"))
 
-	// Use Fprintf to concatenate a string to the Buffer.
-	fmt.Fprintf(&b, "World!")
+	// Fprintf 用于向一个输出通道中追加字符串.
+	fmt.Fprintf(&b, " World!")
 
-	// Write the content of the Buffer to stdout.
+	// 将后者的内容拷贝到前者.
 	io.Copy(os.Stdout, &b)
+
+	// 新增的演示示例
+	useWithPointer()
+}
+
+// 使用指针引用类型的演示
+func useWithPointer()  {
+	b := &bytes.Buffer{}
+	b.Write([]byte("Hello"))
+
+	fmt.Fprintf(b, " World!")
+	io.Copy(os.Stdout, b)
 }
