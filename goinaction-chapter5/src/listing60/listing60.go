b4132400577a411aeafbbdfff11e4dc347deba72
kingsmiler <kingsmiler@163.com>
2016-03-31 18:49:14 +0800

    listing60， 子类对父类接口实现的覆盖

--- goinaction-chapter5/src/listing60/listing60.go
+++ goinaction-chapter5/src/listing60/listing60.go
@@ -1,70 +1,54 @@
-// Sample program to show what happens when the outer and inner
-// type implement the same interface.
+// 子类对父类接口实现的覆盖。
 package main
 
 import (
-	"fmt"
+    "fmt"
 )
 
-// notifier is an interface that defined notification
-// type behavior.
 type notifier interface {
-	notify()
+    notify()
 }
 
-// user defines a user in the program.
+// 父类
 type user struct {
-	name  string
-	email string
+    name, email string
 }
 
-// notify implements a method that can be called via
-// a value of type user.
+// 父类实现
 func (u *user) notify() {
-	fmt.Printf("Sending user email to %s<%s>\n",
-		u.name,
-		u.email)
+    fmt.Printf("Sending user email to %s<%s>\n", u.name, u.email)
 }
 
-// admin represents an admin user with privileges.
+// 子类
 type admin struct {
-	user
-	level string
+    user
+    level string
 }
 
-// notify implements a method that can be called via
-// a value of type Admin.
+// 子类实现
 func (a *admin) notify() {
-	fmt.Printf("Sending admin email to %s<%s>\n",
-		a.name,
-		a.email)
+    fmt.Printf("Sending admin email to %s<%s>\n", a.name, a.email)
 }
 
-// main is the entry point for the application.
 func main() {
-	// Create an admin user.
-	ad := admin{
-		user: user{
-			name:  "john smith",
-			email: "john@yahoo.com",
-		},
-		level: "super",
-	}
+    ad := admin{
+        user: user{
+            name:  "john smith",
+            email: "john@yahoo.com",
+        },
+        level: "super",
+    }
 
-	// Send the admin user a notification.
-	// The embedded inner type's implementation of the
-	// interface is NOT "promoted" to the outer type.
-	sendNotification(&ad)
+    // 直接调用时，使用的是子类自身的实现
+    sendNotification(&ad)
 
-	// We can access the inner type's method directly.
-	ad.user.notify()
+    // 通过使用父类名称来调用父类的实现
+    ad.user.notify()
 
-	// The inner type's method is NOT promoted.
-	ad.notify()
+    // 直接调用时，使用的是子类自身的实现
+    ad.notify()
 }
 
-// sendNotification accepts values that implement the notifier
-// interface and sends notifications.
 func sendNotification(n notifier) {
-	n.notify()
+    n.notify()
 }
